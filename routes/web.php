5e5d1f736c2e98fd56f276f69de203816d4fb61e
Yousaf Arshad <yousaf.arshad@sca.sa>
2025-05-25 11:46:04 +0300

    sanctum installed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +11,9 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken('initiate-contract');
+    return ['token' => $token->plainTextToken]; // use as a Bearer token in the Authorization header
+})->middleware(['auth', 'verified'])->name('create-token');
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
